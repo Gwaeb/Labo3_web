5f6e43767567d39a4786acb2da1686a1112eff97
etd <etd@debian>
2021-11-03 08:50:06 -0400

    mis en place du root sur le /

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 #Gabriel Meunier 20/10/21
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/', to: 'public#showall'
+  root to: 'public#showall'
   get '/recettes/:id', to: 'public#showid'
   get '/admin/recettes', to: 'admin#showall'
   get '/admin/recettes/:id', to: 'admin#showid'
